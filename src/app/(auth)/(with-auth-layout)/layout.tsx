2d10e2cf9dcf4ac204cf245c123573e3808fe128
sinhu-jung <tlsgn26@naver.com>
2024-07-31 22:54:56 +0900

    Active Links

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,13 +1,35 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import './style.css';
+
+const navLinks = [
+  { name: 'Register', href: '/register' },
+  { name: 'Login', href: '/login' },
+];
+
 export default function ProductsLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-     <>
-        <h2>Inner layout</h2>
-        {children}
-     </>
-    )
-  }
-  
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathName = usePathname();
+
+  return (
+    <>
+      {navLinks.map((link) => {
+        const isActive = pathName.startsWith(link.href);
+        return (
+          <Link
+            href={link.href}
+            key={link.name}
+            className={isActive ? 'font-bold mr-4' : 'text-blue-500 mr-4'}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </>
+  );
+}
