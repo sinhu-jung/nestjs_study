073686f851347bdf9273beefecc34b48ce1adcb9
sinhu-jung <tlsgn26@naver.com>
2024-07-31 22:43:57 +0900

    prettier setting and link component navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import * as React from "react";
+import Link from 'next/link'
+import * as React from 'react'
 
 export interface IAppProps {}
 
 export default function Home(props: IAppProps) {
-  return <h1>Welcome Home</h1>;
+  return (
+    <>
+      <h1>Welcome Home</h1>
+      <Link href={'/blog'}>Blog</Link>
+      <Link href={'/products'}>Products</Link>
+    </>
+  )
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,25 @@
+import Link from 'next/link'
+
 export default function ProductList() {
+  const productId = 100
   return (
     <>
+      <Link href={'/'}>Home</Link>
       <h1>Product List</h1>
-      <h2>Product1</h2>
-      <h2>Product2</h2>
-      <h2>Product3</h2>
+      <h2>
+        <Link href={'products/1'}>Product1</Link>
+      </h2>
+      <h2>
+        <Link href={'products/2'}>Product2</Link>
+      </h2>
+      <h2>
+        <Link href={'products/3'} replace>
+          Product3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>Product{productId}</Link>
+      </h2>
     </>
-  );
+  )
 }
