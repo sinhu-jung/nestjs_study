2b8369cd5ae416d474b91a77324b11a5f9b5ce9e
sinhu-jung <tlsgn26@naver.com>
2024-07-31 22:27:51 +0900

    title metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from "next";
+
+/**
+ * 메인 레이아웃에 title.template 에 설정을 해놔도 absolute 로 잡으면 absolute가 먹힌다.
+ */
+export const metadata: Metadata = {
+  title: {
+    absolute: 'Blog'
+  }
+}
+
 export default function Blog() {
   return <h1>My blog</h1>;
 }
