7df4dfc519334d1ad018c209cd7aad12072ad752
sinhu-jung <tlsgn26@naver.com>
2024-07-31 19:35:39 +0900

    not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) notFound();
+
   return (
     <>
       <h1>
