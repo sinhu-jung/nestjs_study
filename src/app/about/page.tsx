05e64d2b266deb8088187ff90d34854befe0b73d
sinhu-jung <tlsgn26@naver.com>
2024-08-02 14:17:54 +0900

    streaming

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
+import { cookies } from 'next/headers';
+
 export default function AboutPage() {
+  const cokieStore = cookies();
+  const theme = cokieStore.get('theme');
+
+  console.log(theme);
   console.log('About Server Component');
 
   return <h1>About Page</h1>;
