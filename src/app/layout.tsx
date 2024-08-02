79962767647f9211fbaa746dad2566d682bbdcac
sinhu-jung <tlsgn26@naver.com>
2024-08-02 17:20:19 +0900

    data fetching, error, loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -17,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <ThemeProvider>
-        <body className={inter.className}>{children}</body>
-      </ThemeProvider>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
