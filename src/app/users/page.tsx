c65a9d733157aa33f32da2f18c212d1d599f4077
sinhu-jung <tlsgn26@naver.com>
2024-08-03 13:08:00 +0900

    data fetching

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -8,7 +8,7 @@ type User = {
 
 export default async function UsersPage() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch('https://jsonplaceholder.typicode.com/users123');
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await response.json();
   return (
     <div className="grid grid-cols-2 gap-2 p-4">
